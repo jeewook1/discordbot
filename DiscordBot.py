c5be1546de5ae6fa7474db325d92e9b22d418e4d
jeewook1 <56481805+jeewook1@users.noreply.github.com>
2019-10-14 23:36:44 +0900

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -54,5 +54,5 @@ async def on_message(message):
                     break
                 i += 1
 
-access_token = os.environ["BOT_TOKEN]
+access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
